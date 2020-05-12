b6ab2780cc226ef07d6579ee12910d4d0c43baf8
D'onta Hobson <hobsond26@icloud.com>
2020-05-12 19:04:27 -0400

    finished mvp requirements

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import {connect} from 'react-redux'
+import {AddFeature,addFeature} from '../actions/addFeature'
+
 
 const AdditionalFeature = props => {
+  const updateFeature = e => {
+    e.preventDefault()
+    props.addFeature(props.feature)
+  }
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={updateFeature} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
+const mapToState =(state)=>{
+  return{
+    state
+  }
+}
 
-export default AdditionalFeature;
+export default connect(mapToState,{addFeature})(AdditionalFeature);
